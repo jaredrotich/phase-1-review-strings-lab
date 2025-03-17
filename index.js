b1554c3e47458e77a983dd0b960b959e79f7612f
jared <jaredrotich3@gmail.com>
2025-03-17 13:25:16 +0300

    commit change

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!'
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+'Edsger Dijkstra'.slice();
+
+'Edsger Dijkstra'.slice(7);
+
+'Edsger Dijkstra'.slice(0, 3);
+
+currentUser.slice(0, 1);
+
+const shortGreeting = `welcome, ${currentUser. slice(0, 1)}!`;
